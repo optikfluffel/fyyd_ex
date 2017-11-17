77403ffdd9489776c57b8fe86e969ba715dcb1e9
optikfluffel <optik@fluffel.io>
2017-11-17 20:38:18 +0100

    init project with `mix new fyyd_ex`

---
+++ lib/fyyd_ex.ex
@@ -0,0 +1,18 @@
+defmodule FyydEx do
+  @moduledoc """
+  Documentation for FyydEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FyydEx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fyyd_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule FyydExTest do
+  use ExUnit.Case
+  doctest FyydEx
+
+  test "greets the world" do
+    assert FyydEx.hello() == :world
+  end
+end
