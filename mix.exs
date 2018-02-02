b98c7ed944d36e9ca12062983554ec4828a3cc3e
optikfluffel <optik@fluffel.io>
2018-02-02 13:09:27 +0100

    downgrade httpoison to 0.13 to get rid of override
    > (Mix) Can't build package with overridden dependency httpoison, remove `override: true`

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule FyydEx.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.0", override: true},
+      {:httpoison, "~> 0.13"},
       {:poison, "~> 3.1"},
       # dev tools
       {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
