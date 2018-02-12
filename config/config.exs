66b869a52c92d6e49d8b168f9b8b55cce273bf9b
optikfluffel <optik@fluffel.io>
2018-02-12 13:59:45 +0100

    disable tzdata’s autoupdate when testing
    to prevent Travis from failing in some cases 🙄

--- config/config.exs
+++ config/config.exs
@@ -30,6 +30,10 @@ if Mix.env() == :dev do
     ]
 end
 
+if Mix.env() == :test do
+  config :tzdata, :autoupdate, :disabled
+end
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
