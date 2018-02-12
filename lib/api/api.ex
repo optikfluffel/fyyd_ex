e2d580ff5adb07c38301be87c8084e1438249ec6
optikfluffel <optik@fluffel.io>
2018-02-12 13:49:05 +0100

    refactor get_data

--- lib/api/api.ex
+++ lib/api/api.ex
@@ -12,14 +12,22 @@ defmodule Fyyd.API do
   @api_version "0.2"
   @base_url "https://api.fyyd.de"
 
-  @spec get_data(String.t()) :: {:ok, map} | {:ok, [map]} | {:error, :not_found}
-  def get_data(url) do
-    case get(url) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: %Response{data: data}}} ->
-        {:ok, data}
+  @doc """
+  Get and unwrap data from the Fyyd HTTP API for the given `url`.
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, :not_found}
+  ## Usage
+
+      {:ok, user_data} = Fyyd.API.get_data("/user?nick=optikfluffel")
+
+  """
+  @spec get_data(String.t()) :: {:ok, map} | {:ok, [map]} | {:error, any}
+  def get_data(url) do
+    with {:ok, response} <- get(url),
+         {:ok, data} <- handle_response(response) do
+      {:ok, data}
+    end
+  end
+
     end
   end
 
@@ -37,5 +45,21 @@ defmodule Fyyd.API do
         }
       }
     )
+  end
+
+  # ----------------------------------------Private Helper
+  # Unwraps the response data from a given `%HTTPoison.Response{}`.
+  defp handle_response(%HTTPoison.Response{status_code: 200, body: %Response{data: data}}) do
+    {:ok, data}
+  end
+
+  defp handle_response(%HTTPoison.Response{status_code: 404}) do
+    {:error, :not_found}
+  end
+
+  defp handle_response(response) do
+    _ = Logger.error("[Fyyd.API] [handle_response] UNKNOWN RESPONSE")
+    _ = Logger.error(inspect(response))
+    {:error, :unknown}
   end
 end
